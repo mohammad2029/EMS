a5fce6052dc72e0b3f2930ef4a3b08e25156ca4d
mohammad kn3 <mohammadkn3an2001@gmail.com>
2024-08-22 15:18:20 +0300

    delet testing func

--- EventMangmentSystem/routes/web.php
+++ EventMangmentSystem/routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('hello', [testController::class,'hello']);
+// Route::get('hello', [testController::class,'hello']);
